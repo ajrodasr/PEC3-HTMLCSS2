4e1968b53869b17ce57a1cf219930ad1d3e4ca21
ajrodasr <er.rodas91@gmail.com>
2021-05-17 20:48:37 +0200

    Portada tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   purge: ['./src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#873535',
+        'primary-bg': '#e4baba',
+        secondary: '#000',
+        white: '#fff',
+        'white-hover': '#ffffffc9',
+        'white-poster': 'rgb(255 255 255 / 10%)',
+      },
+      backgroundImage: (theme) => ({
+        'portada-poster': "url('../images/portada/poster.jpg')",
+        members: "url('../images/portada/members.jpg')",
+        bio: "url('../images/portada/bio.jpg')",
+        contact: "url('../images/portada/contacto.jpg')",
+      }),
+      boxShadow: {
+        'poster-top': '2px 2px 2px black',
+        'poster-bottom-right': '-2px -2px 2px black',
+        'poster-bottom-left': '2px 0px 2px black',
+      },
+      lineHeight: {
+        100: '100px',
+      },
+    },
   },
   variants: {
     extend: {},
